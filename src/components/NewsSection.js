033d935d40eafeef6c92855a81f878d7cc0690d8
agusbustadev <agustin.bustamante@chewedpixelstudios.com>
2024-04-02 00:05:22 -0300

    2

--- src/components/NewsSection.js
+++ src/components/NewsSection.js
@@ -7,7 +7,7 @@ function NewsSection({ news }) {
       <br />
       <br />
       <div>
-        <h2>All X Payments News</h2>
+        <h2>All Defi News</h2>
         <hr />
         {news.map((item, index) => (
           <NewsItem
